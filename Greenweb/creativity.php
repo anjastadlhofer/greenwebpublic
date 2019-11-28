bf43ab918f022f05cef6174ed11e5994ea71bb39
Julia Gruber <julia.gruber@edu.fh-joanneum.at>
2019-11-28 12:39:33 +0100

    computer Counter
    computer Counter

--- Greenweb/creativity.php
+++ Greenweb/creativity.php
@@ -7,14 +7,19 @@
             
             <h3 class="mb-0">text</h3>
             <div class="subheading mb-3">text</div>
-              Telefone produziert: <span id="mobilephoneCounter"></span>
-              <br>
-              Quelle: gartner.com(1943 Mio Telefone pro Jahr)
+              
             
             <p>
               
               <!-- text -->
                 
+                Telefone produziert: <span id="mobilephoneCounter"></span>
+              <br>
+              Quelle: gartner.com(1943 Mio Telefone pro Jahr)
+              <br><br>
+                Telefone produziert: <span id="computerCounter"></span>
+              <br>
+              Quelle: gartner.com(473 Mio Computer pro Jahr)
                 
                 
 
@@ -41,6 +46,16 @@
                 else
                     clearInterval(inv2);
             }   , 15);
+        </script>
+        
+         <script>
+            var i3 = 0;
+            var inv3 = setInterval(function() {
+                if(i3 < 473000000)
+                    document.getElementById("computerCounter").innerHTML = ++i3;
+                else
+                    clearInterval(inv3);
+            }   , 90);
         </script>
 
     </section>
