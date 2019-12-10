039de8ee453061b14066f42d96d9bfa22ddb12ca
tripoltc17 <45813872+tripoltc17@users.noreply.github.com>
2019-12-10 15:16:32 +0100

    quiz margin

--- Greenweb/quiz.php
+++ Greenweb/quiz.php
@@ -4,12 +4,14 @@
   background: #01450b;
   color: #fff;
   padding: 10px;
-  border: 0;
+  border: 10px;
   padding: 10px 20px;
-  margin-top: 10px;
+  margin: 10px;
   font-size: 20px;
 }
-    
+    .oquiz{
+        margin:10px;
+    }  
 
 
 </style>
