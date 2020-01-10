687c23113ba8f4ce8627ce6de3e92751d1729c39
anjastadlhofer <anja.stadlhofer@gmail.com>
2020-01-10 10:07:52 +0100

    added Impressum incl. pdf Upload

---
+++ Greenweb/impressum.php
@@ -0,0 +1,41 @@
+<hr class="m-0">
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="impressum">
+      <div class="w-100">
+        <h2 class="mb-5">Impressum</h2>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0"></h3>
+            <div class="subheading mb-3"></div>
+           <p>
+              Diese Website wurde im Zuge der <b>Bereichsübergreifende Projektarbeit</b> von <b>IMA17</b> erstellt.<br>
+            Alle Daten für die Berechnungen und Annahmen wurden selbstständig generiert durch einen Aufbau eines Testnetzwerkes. 
+               Nähere Informationen finden Sie in den beiden Dokumentationen:<br>
+               <li>Doku Greenweb</li>
+               <li><a href="pdf/GreenWebPrototype.pdf" download>Dokumentation GreenWebPrototype</a></li>
+               
+            <br> 
+              Projektleitung "GreenWeb": <b>Gerhard Sprung, MSc</b><br>
+              Studierendenteam beim Projekt "GreenWeb" und verantwortlich für diese Website:<br>
+              <li><b>Julia Gruber</b></li>
+              <li><b>Lukas Reitbauer</b></li>
+              <li><b>Anja Stadlhofer</b></li><br>
+             <br>
+              Projektleitung "GreenWebPrototype": <b>DI Georg Mittenecker</b><br>
+              Studierendenteam beim Projekt "GreenWebPrototype" und verantwortlich für die Infrastruktur zur Generierung der Daten:<br>
+              <li><b>Colin Jochum</b></li>
+              <li><b>Marcel Kahr</b></li>
+              <li><b>Tom Kleinhapl</b></li><br><br>
+
+              Dieses Projekt wurde im Auftrag der FH Joanneum erstellt. <br>
+               <a href="https://www.fh-joanneum.at/" target="_blank"><img src="img/FH.jpg" width= 60%></a>
+               
+            </p>  
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary"></span>
+          </div>
+        </div>
+               
+        
+</div>
+</section>
