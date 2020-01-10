403dc1f09242d4d722c66cfc6cf84447d0cb2615
Julia Gruber <julia.gruber@edu.fh-joanneum.at>
2020-01-10 14:07:53 +0100

    Update consume.php

--- Greenweb/consume.php
+++ Greenweb/consume.php
@@ -7,6 +7,59 @@
             padding-bottom: 40px;
             background-color: #C0E9B5; 
     }
+    /* Popup container */
+.popup {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+/* The actual popup (appears on top) */
+.popup .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class when clicking on the popup container (hide and show the popup) */
+.popup .show {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
+
 </style>
 <!---   #d8f0e8;-->
 
@@ -18,8 +71,15 @@
           <div class="resume-content">
             
             <h3 class="mb-0">Produzierte Resourcen</h3>
-              <br>
+              <div class="popup" onclick="myFunction()" align=right>Click
+  <span class="popuptext" id="myPopup">Mit dem Besuch auf dieser Website wurde die beiden Counter gestartet. Bei diesen kann man erkennen wie viele Ressourcen in der Zwischenzeit produziert wurden.</span>
+</div> 
+              
+    <br>
+              
               Seit deinem Besuch auf dieser Website ist auf der Welt so einiges passiert:
+              
+              
             <div class="subheading mb-3"> </div>
               
             
@@ -77,7 +137,13 @@
                     clearInterval(inv3);
             }   , 65);
         </script>
-        
+        <script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
        
 
     </section>
